6650936b0f1a8778b52faad3b4b870299c7959b8
David Rojas <rojas_exp@hotmail.com>
2022-12-28 22:09:33 -0600

    Se colocó la lista de compras y se adecuó para que se cerraran si ambos se abrieran en la vista de mobile

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const iconCarMenu = document.querySelector('.navbar-shooping-cart');
+const shoppingMenu = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+iconCarMenu.addEventListener('click', toggleIconCar);
 
 function toggleDesktopMenu() {
+    const isIconCarClose = shoppingMenu.classList.contains('inactive');
+
+    if(!isIconCarClose) {
+        shoppingMenu.classList.add('inactive');
+        }
+        
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isIconCarClose = shoppingMenu.classList.contains('inactive');
+
+    if(!isIconCarClose) {
+        shoppingMenu.classList.add('inactive');
+        }
+
     mobileMenu.classList.toggle('inactive');
-    console.log ('click');
+}
+
+function toggleIconCar() {
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClose) {
+    mobileMenu.classList.add('inactive');
+    }
+    
+    shoppingMenu.classList.toggle('inactive');
 }
